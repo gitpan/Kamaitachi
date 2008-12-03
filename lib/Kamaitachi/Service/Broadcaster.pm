7e587ced93eb854e315b6726448db9f7862719a6
Daisuke Murase <typester@gmail.com>
2008-12-03 13:04:56 +0000

    Import of TYPESTER/Kamaitachi-0.01 from CPAN.
    gitpan-cpan-distribution: Kamaitachi
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         TYPESTER/Kamaitachi-0.01.tar.gz
    gitpan-cpan-author:       TYPESTER
    gitpan-cpan-maturity:     released

---
+++ lib/Kamaitachi/Service/Broadcaster.pm
@@ -0,0 +1,71 @@
+package Kamaitachi::Service::Broadcaster;
+use Moose::Role;
+
+with qw/Kamaitachi::Service::ChildHandler
+        Kamaitachi::Service::AMFHandler
+       /;
+
+use Kamaitachi::Packet::Function;
+
+sub broadcast {
+    my ($self, $session, $packet) = @_;
+
+    for my $child_session (@{ $self->child }) {
+        next unless defined $child_session;
+        next if $session->id eq $child_session->id;
+        $child_session->io->write($packet);
+    }
+}
+
+sub broadcast_notify_packet {
+    my ($self, $method, $data) = @_;
+
+    my $notify_packet = Kamaitachi::Packet::Function->new(
+        number => 3,
+        type   => 0x14,
+        id     => undef,
+        method => $method,
+        args   => [undef, $data],
+    );
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Kamaitachi::Service::Broadcaster - service role to broadcast packet to others
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 broadcast
+
+=head2 broadcast_notify_packet
+
+=head1 SEE ALSO
+
+L<Kamaitachi>,
+L<Kamaitachi::Service>
+
+=head1 AUTHOR
+
+Daisuke Murase <typester@cpan.org>
+
+Hideo Kimura <hide@cpan.org>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
